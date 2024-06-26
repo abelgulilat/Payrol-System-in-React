d8d88a64fa0b00443fe324189e50396227c5c7bf
Abel Gulilat <78924068+abelgulilat@users.noreply.github.com>
2024-06-26 10:32:41 +0300

    payrol1

---
+++ Notfoundpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notfoundpage = () => {
+  return (
+    <div className='notfound'>
+        <h1 id='four'>404</h1>
+        <h2  >Page Not Found</h2>
+    </div>
+  )
+}
+
+export default Notfoundpage
